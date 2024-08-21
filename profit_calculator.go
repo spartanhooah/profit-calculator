157389a957f5adc55132305212fcba0aca802aff
Ben Frey <spartanhooah@gmail.com>
2024-08-21 14:59:08 -0400

    task: check for invalid input, write outputs to file

--- profit_calculator.go
+++ profit_calculator.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	revenue := prompt("Revenue: ")
@@ -8,6 +11,7 @@ func main() {
 	taxRate := prompt("Tax rate (e.g. 25.8): ")
 
 	earningsBeforeTax, earningsAfterTax, profitRatio := calculateFinancials(revenue, expenses, taxRate)
+	writeOutputToFile(earningsBeforeTax, earningsAfterTax, profitRatio)
 
 	fmt.Println(earningsBeforeTax)
 	fmt.Println(earningsAfterTax)
@@ -20,6 +24,10 @@ func prompt(prompt string) float64 {
 	var input float64
 	fmt.Scan(&input)
 
+	if input <= 0 {
+		panic("Invalid amount entered.")
+	}
+
 	return input
 }
 
@@ -29,4 +37,8 @@ func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, 
 	profitRatio := ebt / profit
 
 	return ebt, profit, profitRatio
+}
+
+func writeOutputToFile(ebt, profit, ratio float64) {
+	os.WriteFile("results.txt", []byte(fmt.Sprintf("EBT: %.2f\nProfit: %.2f\n Ratio: %.3f\n", ebt, profit, ratio)), 0644)
 }
